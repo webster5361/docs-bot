e227999753a6ad9575f493bf4cf9b9765b7c7815
Odinthewanderer <webster5361@gmail.com>
2017-07-15 21:03:14 -0500

    Created base Client command skeleton

--- commands/Client.js
+++ commands/Client.js
@@ -2,8 +2,7 @@ const settings = require('../settings.json');
 exports.run = (client, message, params) => {
   if (!params[0]) {
     const docChannel = message.guild.channels.find("name", "documentation");
-    //console.log(`${docChannel.id}`);
-    docChannel.send("__Client__\nThe main hub for interacting with the Discord API, and the starting point for any bot.\n\n**Constructor** `new Discord.Client(options)`\n\n**Properties**\n`broadcasts`, `browser`, `channels`, `emojis`, `guilds`, `options`, `ping`, `pings`, `presences`, `readyAt`, `readyTimestamp`, `shard`, `status`, `token`, `uptime`, `user`, `users`, `voiceConnections`\n\n**Methods**\n`clearInterval()`, `createVoiceBroadcast()`, `destroy()`, `fetchApplication()`, `fetchInvite()`, `fetchUser()`, `fetchVoiceRegions()`, `fetchWebhook()`, `generateInvite()`, `login()`, `setInterval()`, `setTimeout()`, `sweepMessages()`, `syncGuilds()`\n\n**Events**\n`channelCreate`, `channelDelete`, `channelPinsUpdate`, `clientUserSettingsUpdate`, `debug`, `disconnect`, `emojiCreate`, `emojiDelete`, `emojiUpdate`, `guildBanAdd`, `guildBanRemove`, `guildCreate`, `guildDelete`, `guildMemberAdd`, `guildMemberAvailable`, `guildMemberRemove`, `guildMembersChunk`, `guildMemberSpeaking`, `guildMemberUpdate`, `guildUnavailable`, `guildUpdate`, `message`, `messageDelete`, `messageDeleteBulk`, `messageReactionAdd`, `messageReactionRemove`, `messageReactionRemoveAll`, `messageUpdate`, `presenceUpdate`, `reconnecting`, `resume`, `roleCreate`, `roleDelete`, `roleUpdate`, `typingStart`, `typingStop`, `userNoteUpdate`, `userUpdate`, `voiceStateUpdate`, `warn`\n\n**Docs link:** https://discord.js.org/#/docs/main/stable/class/Client");
+    docChannel.send("");
   } else {
     console.log("coming soon");
   }
