4e6816d7bba81fbbc48e4be15dc7b2467b142f09
Odinthewanderer <webster5361@gmail.com>
2017-07-17 11:13:16 -0500

    Updated skeletons to allow the bot to run properly with the current command handler

---
+++ commands/UserProfile.js
@@ -0,0 +1,33 @@
+const settings = require('../settings.json');
+exports.run = (client, message, params) => {
+  if (!params[0]) {
+    const docChannel = message.guild.channels.find("name", "documentation");
+    docChannel.send("");
+  } else {
+    const docChannel = message.guild.channels.find("name", "documentation");
+    switch(`${params}`) {
+      // Properties
+
+      // Methods
+
+      // Events
+
+      // Default
+      default:
+        break;
+    }
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [''],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'UserProfile',
+  description: 'Displays all the available information about the UserProfile class',
+  usage: 'UserProfile [arg]'
+};
